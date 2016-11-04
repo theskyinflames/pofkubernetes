532f3dc66c9cc7535108d42799df888064c208d0
Jaume <jaumearus@gmail.com>
2016-11-04 18:36:01 +0100

    Working on adding a node to master ...

--- master/initialize_master.sh
+++ master/initialize_master.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+export myip=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}'|sed ':a;N;$!ba;s/\n/ /g')
+echo "Master ip: ${myip}"
+
 # Common initialization
 echo "Doing common initialization ...."
 . /vagrant_data/initialize.sh
 
 # Starting the master
 echo "Starting the master"
-export myip=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')>/vagrant_data/master_ip
 kubeadm init --api-advertise-addresses=$myip
 
 # Installing network pod
@@ -26,6 +28,6 @@ do
    sleep 5
 done
 
-# Save the master token
+# Saving the master token
 echo "Saving the token string..."
 cat /vagrant_data/master.log|grep token|awk '{print $5}'| sed 's/"//g'>/vagrant_data/token
--- start_pof.sh
+++ start_pof.sh
@@ -7,7 +7,7 @@ vagrant up --provider=virtualbox > ../master.log 2>&1
 # Generate token variable
 cd ..
 export KUBERNETES_MASTER_TOKEN=$(cat ./master.log|grep "generated token"|awk '{print $6}'|sed 's/"//g')
-export KUBERNETES_MASTER_IP=$(cat ./master_ip)
+export KUBERNETES_MASTER_IP=$(cat ./master.log|grep "Master ip:"|sed -e 's/==> master: Master ip://g')
 echo "KUBERNETES_MASTER_TOKEN ${KUBERNETES_MASTER_TOKEN}"
 echo "KUBERNETES_MASTER_IP ${KUBERNETES_MASTER_IP}"
 
