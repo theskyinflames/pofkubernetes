ecfdddf55e7075a4664becbbf83f85c26cdb8eaa
theskyinflames <theskyinflames@gmail.com>
2016-11-06 20:45:20 +0100

    nodes added

--- node2/initialize_node2.sh
+++ node2/initialize_node2.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+export KUBERNETES_MASTER_IP=192.168.33.10
+export KUBERNETES_MASTER_TOKEN=cac195.95cea9f0d7478e7d
+echo "KUBERNETES_MASTER_IP ${KUBERNETES_MASTER_IP}"
+echo "KUBERNETES_MASTER_TOKEN ${KUBERNETES_MASTER_TOKEN}"
 
 # Common initialization
 echo "Doing common initialization ...."
 . /vagrant_data/initialize.sh
-
-echo "KUBERNETES_MASTER_IP ${KUBERNETES_MASTER_IP}"
-
-# Takent the token
-echo "KUBERNETES_MASTER_TOKEN ${KUBERNETES_MASTER_TOKEN}"
 
 # Joinig the node to the master
 echo "Joinig the node to the master..."
--- start_pof.sh
+++ start_pof.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
-#export KUBERNETES_MASTER_IP=$(cat ./master.log|grep "Master ip:"|sed -e 's/==> master: Master ip://g')
-export KUBERNETES_MASTER_IP=192.168.33.10
-echo "KUBERNETES_MASTER_IP ${KUBERNETES_MASTER_IP}"
-
 # Starting the master
 echo "Starting the master ..."
 cd ./master
+export KUBERNETES_MASTER_IP=192.168.33.10
+echo "KUBERNETES_MASTER_IP ${KUBERNETES_MASTER_IP}"
 vagrant up --provider=virtualbox > ../master.log 2>&1
 echo "Master started"
+cd ..
 
 # Generate token variable
-cd ..
 export KUBERNETES_MASTER_TOKEN=$(cat ./master.log|grep "generated token"|awk '{print $6}'|sed 's/"//g')
 echo "KUBERNETES_MASTER_TOKEN ${KUBERNETES_MASTER_TOKEN}"
 
+# Setting master's environment variables into node's initalization files
+sed -i -E "s/KUBERNETES_MASTER_IP=(.*)$/KUBERNETES_MASTER_IP=$KUBERNETES_MASTER_IP/g" ./node1/initialize_node1.sh
+sed -i -E "s/KUBERNETES_MASTER_TOKEN=(.*)$/KUBERNETES_MASTER_TOKEN=$KUBERNETES_MASTER_TOKEN/g" ./node1/initialize_node1.sh
+sed -i -E "s/KUBERNETES_MASTER_IP=(.*)$/KUBERNETES_MASTER_IP=$KUBERNETES_MASTER_IP/g" ./node2/initialize_node2.sh
+sed -i -E "s/KUBERNETES_MASTER_TOKEN=(.*)$/KUBERNETES_MASTER_TOKEN=$KUBERNETES_MASTER_TOKEN/g" ./node2/initialize_node2.sh
+sed -i -E "s/KUBERNETES_MASTER_IP=(.*)$/KUBERNETES_MASTER_IP=$KUBERNETES_MASTER_IP/g" ./node3/initialize_node3.sh
+sed -i -E "s/KUBERNETES_MASTER_TOKEN=(.*)$/KUBERNETES_MASTER_TOKEN=$KUBERNETES_MASTER_TOKEN/g" ./node3/initialize_node3.sh
 
-# Add the node 1
+
+# Adding the node 1
 echo "Adding node 1 ..."
 cd ./node1
 vagrant up --provider=virtualbox > ../node1.log 2>&1
 echo "Node 1 added"
+cd ..
 
-# Add the node 2
+# Adding the node 2
 echo "Adding node 2 ..."
 cd ./node2
 vagrant up --provider=virtualbox > ../node2.log 2>&1
 echo "Node 2 added"
+cd ..
 
-# Add the node 3
+# Adding the node 3
 echo "Adding node 3 ..."
 cd ./node3
 vagrant up --provider=virtualbox > ../node3.log 2>&1
 echo "Node 3 added"
+cd ..
