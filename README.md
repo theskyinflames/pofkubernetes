9138d3851e8affd62f2d605c135fe5a60a732612
Jaume <jaumearus@gmail.com>
2016-11-11 18:03:48 +0100

    On work to startup an external Etcd web client

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Implementation of the getting started guide published at [kubeadm](http://kubernetes.io/docs/getting-started-guides/kubeadm/) for [CentOS 7](https://www.centos.org/) using [Virtualbox](https://www.virtualbox.org/) and [Vagrant](https://www.vagrantup.com/).
 
+http://severalnines.com/blog/installing-kubernetes-cluster-minions-centos7-manage-pods-services
+
 ## Dependencies
 * Vagrant version >= 1.8.1
 * Virtualbox version >= 5.1.8
+* Docker version >=1.10 (for the etcd web client)
+
 ## Checking for Etcd is running
 Log into master as root and do:
 ```sh
--- start_pof.sh
+++ start_pof.sh
@@ -23,7 +23,20 @@ sed -i -E "s/KUBERNETES_MASTER_IP=(.*)$/KUBERNETES_MASTER_IP=$KUBERNETES_MASTER_
 sed -i -E "s/KUBERNETES_MASTER_TOKEN=(.*)$/KUBERNETES_MASTER_TOKEN=$KUBERNETES_MASTER_TOKEN/g" ./node2/initialize_node2.sh
 sed -i -E "s/KUBERNETES_MASTER_IP=(.*)$/KUBERNETES_MASTER_IP=$KUBERNETES_MASTER_IP/g" ./node3/initialize_node3.sh
 sed -i -E "s/KUBERNETES_MASTER_TOKEN=(.*)$/KUBERNETES_MASTER_TOKEN=$KUBERNETES_MASTER_TOKEN/g" ./node3/initialize_node3.sh
+#sed -i -E "s/ETCD-HOST=(.*)etcd-browse(.*)$/ETCD-HOST=${KUBERNETES_MASTER_IP} etcd-browser/g" ./start_etcd_browser.sh
 
+# Starting Etcd web browser
+if [[ -d ./etcd-browser ]]; then
+  echo "Removing previous etcd-browser version..."
+  rm -rf ./etcd-browser
+fi
+echo "Downloading etcd-browser ..."
+git clone https://github.com/henszey/etcd-browser.git
+cd etcd-browser
+docker build -t etcd-browser .
+docker run -d --name coregi --name etcd-browser -p 0.0.0.0:8000:8000 --env ETCD-HOST=127.0.0.1 etcd-browser
+cd ..
+echo "Etcd-browser is running."
 
 # Adding the node 1
 echo "Adding node 1 ..."
@@ -45,3 +58,5 @@ cd ./node3
 vagrant up --provider=virtualbox > ../node3.log 2>&1
 cd ..
 echo "Node 3 added"
+
+exit 0
